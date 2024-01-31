79768d06414709155be98578af4274e06bd9e971
Devansh141202 <20ce061@charusat.edu.in>
2024-01-31 17:24:15 +0530

    complete

--- server.js
+++ server.js
@@ -21,29 +21,29 @@ app.post("/api/photos", uploads.single("files"), (req, res) => {
 const { OpenAI } = require("openai");
 
 const openai = new OpenAI({
-    apiKey: "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"
+  apiKey: "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",
 });
 
-app.post('/chatGPT', async (req, res) => {
-    console.log("Hello")
-    const { prompt } = req.body;
-    const completion = await openai.chat.completions.create({
-        messages: [{ role: "user", content: prompt }],
-        model: "gpt-3.5-turbo",
-    });
-    return res.send(completion.choices[0].message.content)
-    // console.log(completion.choices[0].message.content);
-})
+app.post("/chatGPT", async (req, res) => {
+  console.log("Hello");
+  const { prompt } = req.body;
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: prompt }],
+    model: "gpt-3.5-turbo",
+  });
+  return res.send(completion.choices[0].message.content);
+  // console.log(completion.choices[0].message.content);
+});
 
 connectDatabase();
 
 const { spawn } = require("child_process");
-const Image = require("./Models/imageModel")
+const Image = require("./Models/imageModel");
 
 const pythonScriptPath = "./hello.py";
 require("dotenv").config({ path: "./config/.env" });
-const path = require('path');
-const dirPath = path.join(__dirname, 'uploads/');
+const path = require("path");
+const dirPath = path.join(__dirname, "uploads/");
 
 app.use(express.json());
 var cors = require("cors");
@@ -53,30 +53,32 @@ app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.urlencoded({ extended: true }));
 const upload = multer({
-  dest: dirPath,
+  dest: (req, file, cb) => {
+    cb(null, path.join(__dirname, "uploads/"));
+  },
   limits: {
     fileSize: 30 * 1000000,
   },
   fileFilter: (req, file, cb) => {
     const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
-    
+
     if (!allowedTypes.includes(file.mimetype)) {
       const error = new Error("Invalid file type");
       error.code = "INVALID_FILE_TYPE";
       return cb(error, false);
     }
-    
+
     cb(null, true);
   },
 });
-app.get("/get-image/:id", async(req, res)=>{
-  const {id} = req.params
-  const image = await Image.findById(id)
-  if(!image){
-    return res.status(400).send({message: "image not found!!"})
+app.get("/get-image/:id", async (req, res) => {
+  const { id } = req.params;
+  const image = await Image.findById(id);
+  if (!image) {
+    return res.status(400).send({ message: "image not found!!" });
   }
-  return res.status(200).send({message: "image fetched!!",image});
-})
+  return res.status(200).send({ message: "image fetched!!", image });
+});
 app.post("/api/photos", upload.single("files"), (req, res) => {
   const pythonScriptArgs = [req.file.destination, req.file.filename];
   console.log("upload success!!");
@@ -90,28 +92,27 @@ app.post("/api/photos", upload.single("files"), (req, res) => {
       console.log(`Python script output: ${data}`);
       response += data;
     });
-    
+
     pythonProcess.stderr.on("data", (data) => {
       console.error(`Error in Python script: ${data}`);
       return;
     });
-    pythonProcess.on("close", async(code) => {
+    pythonProcess.on("close", async (code) => {
       if (code === 0) {
-        const dbData = await Image.create({image:response})
-        return res.status(200).send({ message: "Image inserted!!",dbData });
+        const dbData = await Image.create({ image: response });
+        return res.status(200).send({ message: "Image inserted!!", dbData });
       } else {
         console.error(`Python script exited with code ${code}`);
         // return res.status(500).json({ error: "Internal server error" });
       }
     });
-    
   } catch (error) {
     console.log(error);
   }
 });
-app.get("/", (req, res) =>{
-  res.send("Server Deployed!!")
-})
+app.get("/", (req, res) => {
+  res.send("Server Deployed!!");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server Listening on ${process.env.PORT}`);
